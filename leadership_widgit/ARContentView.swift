5792a7ec2d6fafccec5d0b61177a25cbb78f6157
i325486 <a1s2d3f4>
2021-01-09 15:03:42 +0100

    ARview

--- leadership_widgit/ARContentView.swift
+++ leadership_widgit/ARContentView.swift
@@ -6,7 +6,11 @@
 //
 import SwiftUI
 import RealityKit
+import SceneKit
+import ARKit
 
+
+private let kBoxWidth: CGFloat = 0.2
 struct ARContentView : View {
     var body: some View {
         return ARViewContainer().edgesIgnoringSafeArea(.all)
@@ -18,21 +22,41 @@ struct ARViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        let boxAnchor2 = try! Experience.loadBox()
-        
+//
+//        // Load the "Box" scene from the "Experience" Reality File
+//        let boxAnchor = try! Experience.loadBox()
+//        let blocks = try! BlockStops.loadBox()
+
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+//        let anchor = try! BlockStops.loadBox()
+//        let anchor2 = try! Circlestops.loadBox()
+//        let anchor3 = try! Experience.loadBox()
+//
+//        anchor2.setPosition(SIMD3<Float>(6, 6, 0), relativeTo: anchor)
+//
+//
+//        arView.scene.anchors.append(anchor)
+//
+//
+//        arView.scene.anchors.append(anchor2)
+//
+//        arView.scene.anchors.append(anchor3)
+        let anchor = try! BlockStops.loadBox()
         
+        arView.scene.anchors.append(anchor)
+        
+        updateUIView(arView, context: context)
         
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        let box = try! Experience.loadBox()
+
+        uiView.scene.anchors.append(box)
+        
+    }
     
 }
 
